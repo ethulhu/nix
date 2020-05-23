d9eb94112c4359a7e28643faa39ea6b061c99c4d
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-23 22:27:38 +0100

    add catbus-snapcast and catbus-lifx to pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,9 @@ pkgs: super: {
   eth = {
     recipesEthMoe = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/recipes.eth.moe"; } ) {};
 
+    catbus-snapcast = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/catbus-snapcast"; } ) {};
+    catbus-lifx     = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/catbus-lifx";     } ) {};
+
     helix = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/helix"; rev = "142be94fb81ee3ff7aba1694836fd45330d22862"; } ) {};
 
     libnpupnp = pkgs.callPackage ./libnpupnp { };
