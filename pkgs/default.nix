dfe65f669311711d035c5e4d52ae93bc430a5e21
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-29 21:17:50 +0100

    update Helix version

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,7 @@ pkgs: super: {
     catbus-snapcast = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/catbus-snapcast"; } ) {};
     catbus-web-ui   = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/catbus-web-ui";   } ) {};
 
-    helix = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/helix"; rev = "142be94fb81ee3ff7aba1694836fd45330d22862"; } ) {};
+    helix = pkgs.callPackage ( builtins.fetchGit { url = "https://github.com/ethulhu/helix"; rev = "6162800302ac41bcacded9916260827bbf5ac99e"; } ) {};
 
     dlnatoad = pkgs.callPackage ./dlnatoad {};
 
